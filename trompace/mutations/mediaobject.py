2b82f521f525e712db151d449b62eaa8cd783c74
pc2752 <pc2752@gmail.com>
2020-03-05 19:03:55 +0100

    Added mutations for media objects creation, updates and deletions

--- trompace/mutations/mediaobject.py
+++ trompace/mutations/mediaobject.py
@@ -4,19 +4,19 @@ from . import StringConstant, _Neo4jDate
 from .templates import mutation_create, mutation_update, mutation_delete
 from ..constants import SUPPORTED_LANGUAGES
 
-CREATE_MEDIAOBJECT = '''CreatePerson(
+CREATE_MEDIAOBJECT = '''CreateMediaObject(
         {parameters}
     ) {{
       identifier
     }}'''
 
-UPDATE_PERSON = '''UpdatePerson(
+UPDATE_MEDIAOBJECT = '''UpdateMediaObject(
       {parameters}
     ) {{
       identifier
     }}'''
 
-DELETE_PERSON = '''DeletePerson(
+DELETE_MEDIAOBJECT = '''DeleteMediaObject(
       {parameters}
     ) {{
       identifier
@@ -24,12 +24,13 @@ DELETE_PERSON = '''DeletePerson(
 
 
 def mutation_create_mediaobject(creator: str, encodingFormat: str, contentUrl: str, embedUrl: str, title: str=None, source: str=None):
-    """Returns a mutation for creating a person object
+    """Returns a mutation for creating a media object.
     Arguments:
         creator: The person, organization or service who created the web resource pertaining to the media object.
         encodingFormat: a mime type representing the format of the encoding. 
         contentUrl: The url of the content.
         embedUrl: The url of the embeded work.
+        title: The title of the page from which the person information was extracted.   
         source: The URL of the web resource to be represented by the node. Defaults to contentUrl if not provided.
     Returns:
         The string for the mutation for creating the media object.
@@ -43,113 +44,55 @@ def mutation_create_mediaobject(creator: str, encodingFormat: str, contentUrl: s
 
     args = {
         "creator": creator,
-        "source": source,
-        "subject": "artist",
-        "description": description,
-        "format": formatin,
-        "language": StringConstant(language.lower()),
+        "encodingFormat": encodingFormat,
+        "contentUrl": contentUrl,
+        "embedUrl": embedUrl,
     }
-    if coverage:
-        args["coverage"] = coverage
+    if title:
+        args["title"] = title
     if date:
-        args["date"] = _Neo4jDate(date)
-    if disambiguatingDescription:
-        args["disambiguatingDescription"] = disambiguatingDescription
-    if relation:
-        args["relation"] = relation
-    if _type:
-        args["type"] = _type
-    if _searchScore:
-        args["_searchScore"] = _searchScore
-    if additionalType:
-        args["additionalType"] = additionalType
-    if alternateName:
-        args["alternateName"] = alternateName
-    if image:
-        args["image"] = image
-    if sameAs:
-        args["sameAs"] = sameAs
-    if url:
-        args["url"] = url
-    if additionalName:
-        args["additionalName"] = additionalName
-    if award:
-        args["award"] = award
-    if birthDate:
-        args["birthDate"] = _Neo4jDate(birthDate)
-    if deathDate:
-        args["deathDate"] = _Neo4jDate(deathDate)
-    if familyName:
-        args["familyName"] = familyName
-    if gender:
-        args["gender"] = gender
-    if honorificPrefix:
-        args["honorificPrefix"] = honorificPrefix
-    if honorificSuffix:
-        args["honorificSuffix"] = honorificSuffix
-    if jobTitle:
-        args["jobTitle"] = jobTitle
-    if knowsLanguage:
-        args["knowsLanguage"] = knowsLanguage
-
-    return mutation_create(args, CREATE_PERSON)
+        args["source"] = source
+    return mutation_create(args, CREATE_MEDIAOBJECT)
 
 
-def mutation_update_artist(identifier: str, artist_name=None, publisher=None, contributor=None, creator=None,
-                           source=None, description=None, language=None, coverage=None, date=None,
-                           disambiguatingDescription=None, relation=None, _type=None, _searchScore=None,
-                           additionalType=None, alternateName=None, image=None, sameAs=None, url=None,
-                           additionalName=None,
-                           award=None, birthDate=None, deathDate=None, familyName=None, gender=None, givenName=None,
-                           honorificPrefix=None, honorificSuffix=None, jobTitle=None, knowsLanguage=None):
-    """Returns a mutation for updating a person object
+def mutation_update_mediaobject(identifier: str, encodingFormat: str=None, contentUrl: str=None, embedUrl: str=None, title: str=None, source: str=None):
+    """Returns a mutation for updating a person media object based on the identifier.
     Arguments:
-        identifier: The unique identifier of the artist
-        artist_name (optional): The name of the artist
-        publisher (optional): The person, organization or service responsible for making the artist inofrmation available.
-        contributor (optional): A person, an organization, or a service responsible for contributing the artist to the web resource. This can be either a name or a base URL.
-        creator (optional): The person, organization or service who created the thing the web resource is about.
-        sourcer (optional): The URL of the web resource to be represented by the node.
-        description (optional): An account of the artist.
-        language (optional): The language the metadata is written in. Currently supported languages are en,es,ca,nl,de,fr.
-        coverage (optional): The spatial or temporal topic of the resource, the spatial applicability of the resource, or the jurisdiction under which the resource is relevant.
-        date (optional): A point in time associated with an event in the lifecycle of the resource. Accepts a date type, year(int) or list with [year, month, date] where month and date are optional.
-        disambiguatingDescription (optional): An alternate description of the artist, particularlly to distinguish from other similar artists.
-        relation (optional): A related resource. Any web resource can be used as a relation.
-        _type (optional): The RDF type URI of the node.
-        _searchScore (optional): Unknown, ask Alastair.
-        additionalType (optional): An additional type for the node.
-        alternateName (optional): Alternate name of the artist.
-        image (optional): An image associated with the artist.
-        sameAs (optional): A schema.org property, defined as the URL of a reference Web page that unambiguously indicates the item's identity.
-        url (optional): An additional URL for the artist.
-        additionalName (optional): An additional name for the artist.
-        award (optional): Awards won the artist?
-        birthDate (optional): The birth date of the artist. Accepts a date type, year(int) or list with [year, month, date] where month and date are optional.
-        deathDate (optional): The date of death of the artist. Accepts a date type, year(int) or list with [year, month, date] where month and date are optional.
-        familyName (optional); The family name of the artist.
-        gender (optinal): The artists gender.
-        honorificPrefix (optional): The artist's prefix.
-        honorificSuffix (optional): The artist's suffix.
-        jobTitle (optional): The artist's job title.
-        knowsLanguage (optional): The language known by the artist.
+        identifier: The unique identifier of the media object
+        creator: The person, organization or service who created the web resource pertaining to the media object.
+        encodingFormat: a mime type representing the format of the encoding. 
+        contentUrl: The url of the content.
+        embedUrl: The url of the embeded work.
+        title: The title of the page from which the person information was extracted.   
+        source: The URL of the web resource to be represented by the node. Defaults to contentUrl if not provided.
 
     Returns:
-        The string for the mutation for updating the artist.
+        The string for the mutation for updating the media object.
     Raises:
-        Assertion error if the input language is not one of the supported languages.
+        NotAMimeTypeException: If encodingFormat is not a valid mime type. 
     """
+    args = {"identifier": identifier}
+    if encodingFormat:
+        if "/" not in encodingFormat:
+            raise NotAMimeTypeException(encodingFormat)
+        else: args["encodingFormat"] = encodingFormat
+    if contentUrl:
+        args["contentUrl"] = contentUrl
+    if embedUrl:
+        args["embedUrl"] = embedUrl
+    if title:
+        args["title"] = title
+    if date:
+        args["source"] = source
+    return mutation_create(args, UPDATE_MEDIAOBJECT)
 
-    return mutation_update(identifier, UPDATE_PERSON, artist_name, publisher, contributor, creator, source, description,
-                           language)
 
-
-def mutation_delete_artist(identifier: str):
-    """Returns a mutation for deleting a person object based on the identifier.
+def mutation_delete_mediaobject(identifier: str):
+    """Returns a mutation for deleting a media object based on the identifier.
     Arguments:
-        identifier: The unique identifier of the artist.
+        identifier: The unique identifier of the media object.
     Returns:
-        The string for the mutation for deleting the artist based on the identifier.
+        The string for the mutation for deleting the media object based on the identifier.
     """
 
-    return mutation_delete(identifier, DELETE_PERSON)
+    return mutation_delete(identifier, DELETE_MEDIAOBJECT)
